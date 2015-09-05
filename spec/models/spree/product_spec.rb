c5607ede8f51227474834306c5e0cf039a62b8ad
Gonzalo Moreno <gmoreno@acid.cl>
2015-09-05 19:18:30 -0300

    added spec for product

---
+++ spec/models/spree/product_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.describe Spree::Product, type: :model do
+  describe "searches" do
+    let(:product) { create(:product) }
+
+    before do
+      product.reindex
+      Spree::Product.searchkick_index.refresh
+    end
+
+    it "autocomplete by name" do
+      keyword = product.name[0..3]
+      expect(Spree::Product.autocomplete(keyword)).to eq([product.name.strip])
+    end
+  end
+end
