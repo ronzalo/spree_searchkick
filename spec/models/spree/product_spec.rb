48ed32eca3cb57130bf7c957f88c438ff673c504
Gonzalo Moreno <gmoreno@acid.cl>
2015-11-14 01:06:05 -0300

    reek and rubocop passed in tests

--- spec/models/spree/product_spec.rb
+++ spec/models/spree/product_spec.rb
@@ -1,16 +1,15 @@
 require 'spec_helper'
 
 RSpec.describe Spree::Product, type: :model do
-  describe "searches" do
+  describe 'searches' do
     let(:product) { create(:product) }
 
     before do
       product.reindex
-      Spree::Product.searchkick_index.refresh
       Spree::Product.reindex
     end
 
-    it "autocomplete by name" do
+    it 'autocomplete by name' do
       keyword = product.name[0..3]
       expect(Spree::Product.autocomplete(keyword)).to eq([product.name.strip])
     end
--- spec/routing/spree/products_routes_spec.rb
+++ spec/routing/spree/products_routes_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
 RSpec.describe Spree::ProductsController, type: :routing do
-  describe "routing" do
+  describe 'routing' do
     routes { Spree::Core::Engine.routes }
     let(:taxon) { create(:taxon) }
 
     context 'best selling products' do
-      it "routes to #best_selling" do
-        expect(:get => best_path).to route_to(:controller => "spree/products", :action => "best_selling")
+      it 'routes to #best_selling' do
+        expect(get: best_path).to route_to(controller: 'spree/products', action: 'best_selling')
       end
     end
 
     context 'best selling products by taxon' do
-      it "routes to #create" do
-        expect(:get => best_selling_taxon_path(taxon.permalink)).to route_to(:controller => "spree/products", :action => "best_selling", :id => taxon.permalink)
+      it 'routes to #create' do
+        expect(get: best_selling_taxon_path(taxon.permalink)).to route_to(controller: 'spree/products', action: 'best_selling', id: taxon.permalink)
       end
     end
   end
