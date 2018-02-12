d2be293e77c585b8c8f324b98a80c3a75c1cb059
Artem Kozaev <artemkozaev@gmail.com>
2018-02-12 22:39:25 +0300

    Run test elasticseach cluster in specs

---
+++ spec/support/elasticsearch.rb
@@ -0,0 +1,25 @@
+require 'elasticsearch/extensions/test/cluster'
+
+RSpec.configure do |config|
+  unless ENV['CI']
+    config.before(:suite) { start_elastic_cluster }
+
+    config.after(:suite) { stop_elastic_cluster }
+  end
+end
+
+def start_elastic_cluster
+  ENV['TEST_CLUSTER_PORT'] = '9250'
+  ENV['TEST_CLUSTER_NODES'] = '1'
+  ENV['TEST_CLUSTER_NAME'] = 'spree_searchkick_test'
+
+  ENV['ELASTICSEARCH_URL'] = "http://localhost:#{ENV['TEST_CLUSTER_PORT']}"
+
+  return if Elasticsearch::Extensions::Test::Cluster.running?
+  Elasticsearch::Extensions::Test::Cluster.start(timeout: 10)
+end
+
+def stop_elastic_cluster
+  return unless Elasticsearch::Extensions::Test::Cluster.running?
+  Elasticsearch::Extensions::Test::Cluster.stop
+end
