4e469694113c3b0bf81c4a3b0ab3438e116b544d
Jayaram <jayaramdatti@gmail.com>
2018-03-14 08:41:51 +0530

    Searchkick::Results#records is removed and used Array#max to fix the problem

--- app/helpers/spree/products_helper_decorator.rb
+++ app/helpers/spree/products_helper_decorator.rb
@@ -2,7 +2,7 @@ Spree::ProductsHelper.module_eval do
   def cache_key_for_products
     count = @products.count
     hash = Digest::SHA1.hexdigest(params.to_json)
-    max_updated_at = @products.records.maximum(:updated_at) || Date.today
+    max_updated_at = @products.map(&:updated_at).max || Date.today
     "#{I18n.locale}/#{current_currency}/spree/products/all-#{params[:page]}-#{hash}-#{count}-#{max_updated_at.to_s(:number)}"
   end
 end
